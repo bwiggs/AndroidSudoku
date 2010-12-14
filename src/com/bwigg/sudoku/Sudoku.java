56002673a3c6db9be3a75606a929e557c14447d9
Brian Wigginton <brianwigginton@gmail.com>
2010-12-14 16:50:06 -0700

    added settings screen

--- src/com/bwigg/sudoku/Sudoku.java
+++ src/com/bwigg/sudoku/Sudoku.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Sudoku extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
@@ -16,12 +19,33 @@ public class Sudoku extends Activity implements OnClickListener {
         // Setup click events for all listeners
         View continueButton = findViewById(R.id.continue_button);
         continueButton.setOnClickListener(this);
+        
         View newButton = findViewById(R.id.new_button);
         newButton.setOnClickListener(this);
+        
         View aboutButton = findViewById(R.id.about_button);
         aboutButton.setOnClickListener(this);
+        
         View exitButton = findViewById(R.id.exit_button);
         exitButton.setOnClickListener(this);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflator = getMenuInflater();
+    	inflator.inflate(R.menu.menu, menu);
+    	return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch(item.getItemId()) {
+    	case R.id.settings:
+    		startActivity(new Intent(this, Prefs.class));
+    		return true;
+    	}
+    	return false;
     }
     
     public void onClick(View v) {
